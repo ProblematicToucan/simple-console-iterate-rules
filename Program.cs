86bed0d23c936f7abfe8ad275e668b91d35067af
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2024-10-25 16:01:39 +0700

    feat: Update console for dynamic user input

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using formulatrix_submission_01;
 
-const int x = 15;
-Service.Calculate(x);
+Console.Write("Enter a number: ");
+if (int.TryParse(Console.ReadLine(), out int x) && x > 0)
+{
+    Service.Calculate(x);
+}
+else
+{
+    Console.WriteLine("Please enter a valid positive integer.");
+}
