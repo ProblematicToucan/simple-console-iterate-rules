1968597337b7372792d191f2f1f97329b9d89b0d
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2024-10-25 15:49:27 +0700

    feat: Add foo bar calculate method

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using formulatrix_submission_01;
+
+const int x = 15;
+Service.Calculate(x);
---
+++ Service.cs
@@ -0,0 +1,30 @@
+using System.Text;
+
+namespace formulatrix_submission_01
+{
+    class Service
+    {
+        public static void Calculate(int x)
+        {
+            var result = new StringBuilder();
+            for (int i = 1; i <= x; i++)
+            {
+                bool divisibleBy3 = i % 3 == 0;
+                bool divisibleBy5 = i % 5 == 0;
+
+                if (divisibleBy3 && divisibleBy5)
+                    result.Append("foobar");
+                else if (divisibleBy3)
+                    result.Append("foo");
+                else if (divisibleBy5)
+                    result.Append("bar");
+                else
+                    result.Append(i);
+
+                if (i < x) result.Append(", ");
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
